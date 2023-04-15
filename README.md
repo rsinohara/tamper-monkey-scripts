bd845dc7995bb5375918e66c9758f7844b2696cd
Renato Sinohara <rsinohara@hotmail.com>
2023-04-15 15:56:27 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# tamper-monkey-scripts
-My tamper monkey scripts
+# My tamper monkey user scripts
+A collection of my tamper monkey scrips
+
+## How to use
+
+### Browser extension
+You need the tamper monkey user extension manager extension installed on your browser.
+
+
+[Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+[Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+
+### My user scripts
+
+Once you have the extension installed, just choose a script you would like to add and click on the link below. The extension should take you to an install page.
+
+Script name | Description | Link
+--- | --- | ---
+Speed Curve chart highlight | Allows you to highlight a chart in Speed Curve through the chart title by either hovering it (temporary) or clicking on it (permanent until clicked again) | [link] (https://github.com/rsinohara/tamper-monkey-scripts/raw/master/scripts/speedCurveHighlight.user.js)
---
+++ scripts/speedCurve.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name          Speed Curve chart highlighter
+// @namespace     https://github.com/rsinohara/tamper-monkey-scripts
+// @author        Renato Sinohara
+// @description   Allows you to highlight a chart in Speed Curve by hovering or clicking on the chart title.
+// @run-at        document-start
+// @grant         none
+// @include       *://*app.speedcurve.com/*
+// @version       1.0
+// ==/UserScript==
+
+const styles = `.highcharts-series 
+                {
+                   opacity: 1;           
+                }            
+                .highcharts-root.has-highlight .highcharts-series:not(.highlight, .permanent-highlight),  
+                .highcharts-root.has-highlight .highcharts-series:not(.highlight, .permanent-highlight) 
+                {                
+                  opacity: 0.1    
+                }                 
+                .stat.permanent-highlight 
+                {         
+                  text-decoration: underline          
+                }`;
+
+
+const styleSheet = document.createElement("style");
+styleSheet.innerText = styles;
+document.head.appendChild(styleSheet);
+
+const getIndexFromHeaderElement = (headerElement) =>
+  Array.prototype.indexOf.call(
+    headerElement.parentElement.children,
+    headerElement
+  );
+const findSerieFromHeader = (headerElement) => {
+  const valueIndex = getIndexFromHeaderElement(headerElement);
+  return headerElement.parentElement.parentElement.querySelectorAll(
+    ".highcharts-series-" + valueIndex
+  );
+};
+const detectHighlights = (seriesContainer) => {
+  if (
+    seriesContainer.querySelectorAll(".highlight, .permanent-highlight")
+      .length === 0
+  ) {
+    seriesContainer.classList.remove("has-highlight");
+  } else {
+    seriesContainer.classList.add("has-highlight");
+  }
+};
+document.querySelectorAll(".stat").forEach((headerElement) => {
+  const headerSeries = findSerieFromHeader(headerElement);
+  const seriesContainer = headerSeries[0].parentElement.parentElement;
+  headerElement.addEventListener("mouseenter", () => {
+    headerSeries.forEach((c) => c.classList.add("highlight"));
+    seriesContainer.classList.add("has-highlight");
+  });
+  headerElement.addEventListener("mouseleave", () => {
+    headerSeries.forEach((c) => c.classList.remove("highlight"));
+    detectHighlights(seriesContainer);
+  });
+  headerElement.addEventListener("click", () => {
+    headerElement.classList.toggle("permanent-highlight");
+    headerSeries.forEach((c) => c.classList.toggle("permanent-highlight"));
+    detectHighlights(seriesContainer);
+  });
+});
